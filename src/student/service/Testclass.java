4f7c181ad25a40832f6835b85183e1330bef1906
Bhagya-4310 <157447593+Bhagya-4310@users.noreply.github.com>
2024-06-11 14:52:52 +0530

    commit2

--- src/student/service/Testclass.java
+++ src/student/service/Testclass.java
@@ -8,7 +8,33 @@ import java.util.List;
 
 public class Testclass {
     public static void main(String[] args) {
-        Subject[] sub=new Subject();
+
+
+        Subject[] sub=new Subject[6];
+        sub[0]=new Subject();
+        sub[0].subjectName="a";
+        sub[0].marks=40;
+        sub[1]=new Subject();
+        sub[1].subjectName="b";
+        sub[1].marks=50;
+        sub[2]=new Subject();
+        sub[2].subjectName="c";
+        sub[2].marks=40;
+        sub[3]=new Subject();
+        sub[3].marks=50;
+        sub[3].subjectName="d";
+        sub[4]=new Subject();
+        sub[4].subjectName="e";
+        sub[4].marks=50;
+        sub[5]=new Subject();
+        sub[5].marks=60;
+        sub[5].subjectName="f";
+        Student st=new Student();
+        st.name="bhagi";
+        st.subjects=sub;
+        StudentTest studentTest=new StudentTest();
+        studentTest.result(st);
+
 
        /* List<Subject> sublist=new ArrayList<>();
 
